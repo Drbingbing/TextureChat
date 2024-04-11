2a31af0443bb8cf54112db7b64561a8142b117f8
Drbingbing <tw718140@gmail.com>
2024-04-11 15:50:19 +0800

    project init

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '16.0'
+
+target 'TextureChat' do
+  
+  use_frameworks!
+
+  pod 'Texture'  
+
+end
---
+++ TextureChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextureChat
+//
+//  Created by Bing Bing on 2024/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
