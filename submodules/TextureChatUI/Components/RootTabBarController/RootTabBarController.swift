15653230e91d717b4868f5f9b791a27d522ae003
Drbingbing <tw718140@gmail.com>
2024-04-12 10:18:19 +0800

    remove main storyboard.

--- submodules/TextureChatUI/Components/RootTabBarController/RootTabBarController.swift
+++ submodules/TextureChatUI/Components/RootTabBarController/RootTabBarController.swift
@@ -8,4 +8,10 @@
 import Foundation
 import UIKit
 
-public final class RootTabBarController: UITabBarController {}
+public final class RootTabBarController: UITabBarController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
